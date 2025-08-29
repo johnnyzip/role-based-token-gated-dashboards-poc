26455a8902dccc7798692bd65e65f7dce94bcbc9
johnnyzip <127410610+johnnyzip@users.noreply.github.com>
2025-08-29 21:27:50 +0100

    Update prismaClient.ts

--- web/app/api/prismaClient.ts
+++ web/app/api/prismaClient.ts
@@ -1,9 +1,13 @@
-// web/app/api/prismaClient.ts
 import { PrismaClient } from "@prisma/client";
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient };
+const globalForPrisma = globalThis as unknown as { prisma?: PrismaClient };
 
-export const prisma = globalForPrisma.prisma || new PrismaClient();
+const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    log: process.env.NODE_ENV === "production" ? [] : ["query", "error", "warn"],
+  });
+
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
 export default prisma;
